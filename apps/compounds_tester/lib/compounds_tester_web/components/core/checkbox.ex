a648d512a48355a9e4df2e31a5b9ba05b7f66fcd
phan <thisisphan@gmail.com>
2023-11-11 16:25:03 -0500

    before adding tails dependency

--- apps/compounds_tester/lib/compounds_tester_web/components/core/checkbox.ex
+++ apps/compounds_tester/lib/compounds_tester_web/components/core/checkbox.ex
@@ -7,24 +7,11 @@ defmodule CompoundsTesterWeb.Core.Checkbox do
   alias Phoenix.LiveView.JS
 
 
-  attr :style, :string,
-    default: """
-      .checkbox {
-        height: 1rem;
-        width: 1rem;
-        border: 1px solid #000; 
-        border-radius: 4px
-      }
-    """
-
   attr :checked, :boolean, default: false
-  attr :class, :string, default: "checkbox"
+  attr :class, :string
 
   def checkbox(assigns) do
     ~H"""
-    <style>
-      <%= @style %>
-    </style>
     <input class={@class} type="checkbox" checked={@checked} />
     """
   end
