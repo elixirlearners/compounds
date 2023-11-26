37e22257b27d4a10a59cb9207fe8687396559b78
phan <thisisphan@gmail.com>
2023-11-25 20:55:52 -0500

    fixes

--- apps/doc_site/lib/doc_site_web/components/button_example.ex
+++ apps/doc_site/lib/doc_site_web/components/button_example.ex
@@ -9,36 +9,46 @@ defmodule DocSiteWeb.ButtonExample do
 
   def render(assigns) do
     ~H"""
-    <Compounds.Button.button type="primary">
-      Primary
-    </Compounds.Button.button>
-    <Compounds.Button.button type="secondary">
-      Secondary
-    </Compounds.Button.button>
-    <Compounds.Button.button type="tertiary">
-      Tertiary
-    </Compounds.Button.button>
-    <Compounds.Button.button type="error">
-      Error
-    </Compounds.Button.button>
-    <Compounds.Button.button type="warning">
-      Warning
-    </Compounds.Button.button>
-    <Compounds.Button.button disabled>
-      Disabled
-    </Compounds.Button.button>
-    <Compounds.Button.button disabled>
-      <div class="flex flex-row justify-evenly items-center">
-        <Compounds.Spinner.spinner class="w-7 h-7" />
-        <p>Loading</p>
-      </div>
-    </Compounds.Button.button>
-    <Compounds.Button.button type="secondary" class="p-3">
-      <CoreComponents.icon name="hero-archive-box" />
-    </Compounds.Button.button>
-    <Compounds.Button.button type="secondary" class="p-3">
-      <CoreComponents.icon name="hero-adjustments-horizontal" />
-    </Compounds.Button.button>
+    <div class="flex flex-row items-center space-x-4">
+      <Compounds.Button.button type="primary">
+        Primary
+      </Compounds.Button.button>
+
+      <Compounds.Button.button type="secondary">
+        Secondary
+      </Compounds.Button.button>
+
+      <Compounds.Button.button type="tertiary">
+        Tertiary
+      </Compounds.Button.button>
+
+      <Compounds.Button.button type="error">
+        Error
+      </Compounds.Button.button>
+
+      <Compounds.Button.button type="warning">
+        Warning
+      </Compounds.Button.button>
+
+      <Compounds.Button.button disabled>
+        Disabled
+      </Compounds.Button.button>
+
+      <Compounds.Button.button disabled>
+        <div class="flex flex-row justify-evenly items-center h-full">
+          <Compounds.Spinner.spinner class="w-7 h-7" />
+          <p>Loading</p>
+        </div>
+      </Compounds.Button.button>
+
+      <Compounds.Button.button type="secondary" class="p-3">
+        <CoreComponents.icon name="hero-archive-box" />
+      </Compounds.Button.button>
+
+      <Compounds.Button.button type="secondary" class="p-3">
+        <CoreComponents.icon name="hero-adjustments-horizontal" />
+      </Compounds.Button.button>
+    </div>
     """
   end
 end
